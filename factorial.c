1e27d9de46747b36a6055a94e63f0eec1e58b1f4
ossek <=>
2014-10-16 21:21:21 -0500

    memoized factorial

--- factorial.c
+++ factorial.c
@@ -2,11 +2,45 @@
 #include <stdlib.h>
 
 int factorial(int);
+int memoFactorial(int);
+int fact(int,int[]);
 
 int main(int argct, char** fromInput)
 {
 	int from = atoi(fromInput[1]);
-	printf("%d\n",factorial(from));
+	printf("%d\n",memoFactorial(from));
+}
+
+int memoFactorial(int from)
+{
+	if(from == 0)
+	{
+		return 1;
+	}
+
+	//may need to null out this array first
+	int memo[from + 1];
+	for(int i = 0; i < from+1; i++)
+	{
+		memo[i] = -1;
+	}
+	memo[0] = 1;
+	memo[1] = 1;
+	return fact(from,memo);
+}
+
+int fact(int from,int memo[])
+{
+	if(!(memo[from] == -1))
+	{
+		return memo[from];
+	}
+
+	if(memo[from-1] == -1)
+	{
+	        memo[from -1] = fact(from - 1,memo);
+	}
+	return from * memo[from -1];
 }
 
 int factorial(int from)
